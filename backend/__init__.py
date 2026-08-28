f99e7fcb70fd98dc98dbab27cfd87795acc84d97
airone <erwan.ermel@gmail.com>
2026-08-28 03:59:27 +0200

    DEV-002: Exporter RigManager dans backend/__init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,3 +3,4 @@ from .state_store import StateStore
 from .event_log import EventLog
 from .gps_controller import GpsController
 from .trigger_service import TriggerService, TriggerValidationError
+from .rig_manager import RigManager
