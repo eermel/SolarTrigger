e84d90883da208e28cbb0cf78381bee011a3444a
eermel <erwan.ermel@gmail.com>
2026-09-26 01:17:45 +0200

    Update mount registry expectation for generic INDI

--- tests/test_mount_registry.py
+++ tests/test_mount_registry.py
@@ -4,4 +4,4 @@ from plugins.mount import available_plugins
 def test_available_plugins_includes_indi():
     plugins = {item["id"]: item["name"] for item in available_plugins()}
 
-    assert plugins["indi"] == "INDI / EQMod compatible"
+    assert plugins["indi"] == "INDI telescope"
